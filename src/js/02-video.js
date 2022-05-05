e1b869c6dc0b97b751fb12a4c0707739b81cfad5
Stepan <91005125+Stepan-Borchuk@users.noreply.github.com>
2022-05-05 22:42:53 +0300

    9

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -19,3 +19,6 @@ const pageInit = () => {
 addEventListener('DOMContentLoaded', () => {
     pageInit();
 })
+
+
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,25 +6,21 @@ const refs = {
     form: document.querySelector('.feedback-form'),
     email: document.querySelector('.feedback-form input'),
     message: document.querySelector('.feedback-form textarea'),
-};
+    button: document.querySelector('.feedback-form button')};
+
+populateTextarea();
 
 const formData = {};
 
 refs.form.addEventListener('input', throttle(e => {
-
     formData[e.target.name] = e.target.value;
-
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-    return formData
-
+    if (formData.email && formData.message) {
+        refs.button.disabled = false;
+    }
 }, 500));
 
 refs.form.addEventListener('submit', onFormSubmit);
-
-
-
-populateTextarea();
-
 
 function onFormSubmit(evt) {
     evt.preventDefault(); 
@@ -32,23 +28,26 @@ function onFormSubmit(evt) {
     console.log("Email:", refs.email.value);
     console.log("Message:", refs.message.value);
     
-  evt.currentTarget.reset();
+    evt.currentTarget.reset();
+    refs.button.disabled = true;
   localStorage.removeItem(STORAGE_KEY);
 }
 
 function populateTextarea() {
     try {
-    const savedForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
+        const savedForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
-    if (savedForm.email) {
-    refs.email.value = savedForm.email;
-        if (savedForm.message) {
-            refs.message.value = savedForm.message;
+        if (savedForm.email) {
+        refs.email.value = savedForm.email;
+            if (savedForm.message) {
+                refs.message.value = savedForm.message;
+            }
         }
-    }
-
     } catch (error) {
-        return formData
+        return
     };
 }
 
+if (refs.email.value && refs.message.value) {
+    refs.button.disabled = false;
+}
