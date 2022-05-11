2ac53e97c719c4b1b3bba6ea9a42c22451982f6b
Stepan <91005125+Stepan-Borchuk@users.noreply.github.com>
2022-05-11 20:07:24 +0300

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -20,5 +20,17 @@ addEventListener('DOMContentLoaded', () => {
     pageInit();
 })
 
+// player.setCurrentTime(30.456).then(function(seconds) {
+//     // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
 
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
 
