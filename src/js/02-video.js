a9af16d9b86b04a1e6c480f9c19046356cee84ff
Stepan <91005125+Stepan-Borchuk@users.noreply.github.com>
2022-05-11 21:11:26 +0300

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,28 +9,27 @@ player.on('timeupdate', throttle(function (data) {
     
 
 const pageInit = () => {
+    let seconds=0;
      try {
-        player.setCurrentTime(Number(JSON.parse(localStorage.getItem('time'))))
+         seconds = Number(JSON.parse(localStorage.getItem('time')))
+     
      } catch (error) {
-        console.log('Error parse') 
-     }    
+         console.log('Error parse'); 
+         seconds = 0
+    }; 
+
+    player.setCurrentTime(seconds).then(function() {
+        player.play().then(function() {
+            console.log('the video was played');
+        }).catch(function(error) {
+            console.log(error);
+        });
+    });
 }
 
 addEventListener('DOMContentLoaded', () => {
     pageInit();
 })
 
-// player.setCurrentTime(30.456).then(function(seconds) {
-//     // seconds = the actual time that the player seeked to
-// }).catch(function(error) {
-//     switch (error.name) {
-//         case 'RangeError':
-//             // the time was less than 0 or greater than the video’s duration
-//             break;
 
-//         default:
-//             // some other error occurred
-//             break;
-//     }
-// });
 
